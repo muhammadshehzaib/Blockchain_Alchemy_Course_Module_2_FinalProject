be0cbb86bedcad0d1ec68ef08d7278d65fcb99f0
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-03-26 09:34:04 +0500

    Module 2 for alchemy course is completed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,19 +8,22 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "f5e3c85cfa5de75201f22a926445efa5a923aabc46bbcf6111eae90d0c9b10c2";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  const { proof, name, root } = req.body;
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send({
+      message: "Congratulations! You are on the list. You got a toy robot!",
+    });
+  } else {
+    res.send({
+      message: "Sorry, you are not on the list. No toy robot for you :(",
+    });
   }
 });
 
